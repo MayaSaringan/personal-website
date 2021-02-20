cc8b654b9ea03632d5a13d1c591d73dfe0152d9f
Maya Saringan <mayas.saringan@gmail.com>
2021-02-20 16:18:06 -0500

    Fix casing in file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./Components/App";
+import App from "./components/App";
 import "./index.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 ReactDOM.render(
